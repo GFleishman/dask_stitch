7068bddbb48c3efc16d546576b8d7e02c2c6b36c
fleishmang <fleishmang@janelia.hhmi.org>
2021-04-07 18:58:22 -0400

    added setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="dask_stitch",
+    version="0.0.1",
+    author="Greg M. Fleishman",
+    author_email="greg.nli10me@gmail.com",
+    description="Linear blend stitching for map_overlap calls in dask",
+    url="https://github.com/GFleishman/dask_stitch",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'dask',
+        'dask[array]',
+    ]
+)
+
