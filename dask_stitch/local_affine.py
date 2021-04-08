fb24639a472aa60f1470d20fde58698bbb064513
fleishmang <fleishmang@janelia.hhmi.org>
2021-04-08 10:41:58 -0400

    generalized and refactored local_affine

--- dask_stitch/local_affine.py
+++ dask_stitch/local_affine.py
@@ -12,14 +12,13 @@ def position_grid(shape, blocksize):
     return da.rechunk(coords, chunks=tuple(blocksize) + (3,))
 
 
-def affine_to_grid(matrix, grid):
+def affine_to_field(matrix, grid, displacement=True):
     """
     """
 
-    # reformat matrix, keep track of trimmed dimensions
-    ndims = len(matrix.shape)
-    matrix = matrix.astype(np.float32).squeeze()
-    lost_dims = ndims - len(matrix.shape)
+    # ensure 32 bit data type for fields
+    matrix = matrix.astype(np.float32)
+    grid = grid.astype(np.float32)
 
     # apply affine to coordinates
     mm = matrix[:3, :3]
@@ -27,36 +26,24 @@ def affine_to_grid(matrix, grid):
     result = da.einsum('...ij,...j->...i', mm, grid) + tt
 
     # convert positions to displacements
-    result = result - grid
+    if displacement:
+        result = result - grid
 
-    # restore trimmed dimensions
-    if lost_dims > 0:
-        result = result.reshape((1,)*lost_dims + result.shape)
+    # return
     return result
 
 
-def local_affine_to_displacement(shape, spacing, affines, blocksize):
+def local_affines_to_displacement(
+    shape, spacing, affines,
+    blocksize, overlap,
+    displacement=True,
+):
     """
     """
-
-    # define some helpful variables
-    overlaps = list(blocksize // 2)
-    nblocks = affines.shape[:3]
-
-    # adjust affines for block origins
-    for i in range(np.prod(nblocks)):
-        x, y, z = np.unravel_index(i, nblocks)
-        origin = np.maximum(
-            np.array(blocksize) * [x, y, z] - overlaps, 0,
-        )
-        origin = origin * spacing
-        tl, tr = np.eye(4), np.eye(4)
-        a, tl[:3, -1], tr[:3, -1] = affines[x, y, z], origin, -origin
-        affines[x, y, z] = np.matmul(tl, np.matmul(a, tr))
 
     # get a coordinate grid
     grid = position_grid(
-        np.array(blocksize) * nblocks, blocksize,
+        np.array(blocksize) * affine.shape[:3], blocksize,
     )
     grid = grid * spacing.astype(np.float32)
     grid = grid[..., None]  # needed for map_overlap
@@ -66,26 +53,28 @@ def local_affine_to_displacement(shape, spacing, affines, blocksize):
         affines, chunks=(1, 1, 1, 4, 4),
     )
 
-    # strip dummy axis off grid
-    def wrapped_affine_to_grid(x, y):
-        y = y.squeeze()
-        return affine_to_grid(x, y)
+    # strip dummy axes and get field
+    def wrapped_affine_to_field(x, y):
+        return affine_to_field(
+            x.squeeze(), y.squeeze(),
+            displacement=displacement,
+        )
 
     # compute affine transforms as displacement fields, lazy dask arrays
-    blocksize_with_overlaps = tuple(x+2*y for x, y in zip(blocksize, overlaps))
-    coords = da.map_overlap(
-        wrapped_affine_to_grid, affines_da, grid,
-        depth=[0, tuple(overlaps)+(0, 0)],
+    blocksize_with_overlaps = tuple(x+2*y for x, y in zip(blocksize, overlap))
+    fields = da.map_overlap(
+        wrapped_affine_to_field, affines_da, grid,
+        depth=[0, tuple(overlap)+(0, 0)],
         boundary=0,
         trim=False,
         align_arrays=False,
         dtype=np.float32,
-        new_axis=[5,6],
-        chunks=(1,1,1,) + blocksize_with_overlaps + (3,),
+        drop_axis=[4,],
+        chunks=blocksize_with_overlaps+(3,),
     )
 
     # stitch affine position fields
-    coords = stitch_blocks(coords, blocksize)
+    coords = stitch_blocks(fields, blocksize, overlap)
 
     # crop to original shape
     coords = coords[:shape[0], :shape[1], :shape[2]]
